db9512919de9e4b1cfa21e759900c28c34b8164e
Adesh Boudh <90268094+AdeshBoudh@users.noreply.github.com>
2023-02-13 21:21:17 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@all rights reserved</p>
+        </div>
+
+        <div>
+            <h5>Follw Us</h5>
+            <div>
+                <a href="https://www.youtube.com/@adeshboudh" target={'blank'}>YouTube</a>
+                <a href="https://www.isntagram.com/adeshboudh" target={'blank'}>Instagram</a>
+                <a href="https://www.github.com/AdeshBoudh" target={'blank'}>Github</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
